f8f4abb123b46a0e70fddc3bc35ff02dfe4e7bf5
mkato-manaby <mkato.manaby@gmail.com>
2024-02-12 19:47:02 +0900

    作成終了

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import './style.css';
+import { Image, ScrollControls, Scroll, useScroll } from "@react-three/drei"
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
+import './App.css';
 
-export default function App() {
+function Images() {
+  const {width, height } = useThree((state) => state.viewport);
+  const group = useRef();
+  const data = useScroll();
+
+  useFrame(() => {
+    group.current.children[0].material.zoom = 1 + data.range(0, 1/3) / 3;
+    group.current.children[1].material.zoom = 1 + data.range(0, 1/3) / 3;
+    group.current.children[2].material.zoom = 1 + data.range(0, 1/3) / 3;
+    group.current.children[3].material.zoom = 1 + data.range(0, 1/3) / 3;
+  })
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <group ref={group}>
+      <Image url="./img1.jpg" scale={[4, height, 1]} position={[-1, 0, 1]} />
+      <Image url="./img2.jpg" scale={3} position={[2, 0, 1]} />
+      <Image url="./img3.jpg" scale={[1, 3.5, 1]} position={[-2.3, -height, 2]} />
+      <Image url="./img4.jpg" scale={[1.4, 2, 1]} position={[1.3, -height - 0.3, 3.2]} />
+
+    </group>
   );
 }
+
+function App() {
+  return (
+    <Canvas>
+      <ScrollControls pages={2} damping={1} horizontal={false} infinite={false}>
+        <Scroll>
+          <Images />
+        </Scroll>
+        <Scroll html>
+          <h1 style={{ position: "absolute", top: "60vh", left: "1.5em" }}>Be</h1>
+          <h1 style={{ position: "absolute", top: "140vh", left: "40vw" }}>Creative</h1>
+        </Scroll>
+      </ScrollControls>
+    </Canvas>
+  );
+}
+
+export default App;
